44f31da1620222763489966c9e4d363d87fff6b1
Mark Lilback <mark@lilback.com>
2013-01-10 15:46:05 -0500

    import view shows correct project/workspace items

--- source/ios/FileImportViewController.m
+++ source/ios/FileImportViewController.m
@@ -10,9 +10,11 @@
 #import "FileImportViewController.h"
 #import "Rc2Server.h"
 #import "RCProject.h"
+#import "RCWorkspace.h"
+#import "MAKVONotificationCenter.h"
 
 @interface FileImportViewController () <AMNavigationTreeDelegate>
-@property (nonatomic, strong) AMNavigationTreeController *navController;
+@property (nonatomic, strong) AMNavigationTreeController *treeController;
 @property (weak, nonatomic) IBOutlet UIBarButtonItem *cancelButton;
 @property (weak, nonatomic) IBOutlet UIBarButtonItem *doneButton;
 @property (weak, nonatomic) IBOutlet UILabel *importLabel;
@@ -33,43 +35,70 @@
 - (void)viewDidLoad
 {
 	[super viewDidLoad];
-	self.navigationItem.title = @"Import do-hickey";
+	UIBarButtonItem *doneButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemDone target:self action:@selector(doDone:)];
+	self.navigationItem.rightBarButtonItem = doneButton;
+	self.navigationItem.title = @"Import File";
 	NSMutableArray *prjs = [[Rc2Server sharedInstance].projects mutableCopy];
 	for (NSInteger i=prjs.count-1; i>=0; i--) {
 		if (![[prjs objectAtIndex:i] userEditable])
 			[prjs removeObjectAtIndex:i];
 	}
 	self.projects = prjs;
-	self.navController = [[AMNavigationTreeController alloc] init];
-	self.navController.manageNavigationStack = NO;
-	self.navController.delegate = self;
-	self.navController.keyForCellText = @"name";
-	[self.treeContainer addSubview:self.navController.view];
-	self.navController.view.frame = self.treeContainer.bounds;
+	self.treeController = [[AMNavigationTreeController alloc] init];
+	[self addChildViewController:self.treeController];
+	self.treeController.delegate = self;
+	self.treeController.keyForCellText = @"name";
+	self.treeController.navigationItem.rightBarButtonItem = doneButton;
+	[self.treeContainer addSubview:self.treeController.view];
+	self.treeController.view.frame = self.treeContainer.bounds;
 	self.importLabel.text = [NSString stringWithFormat:@"Import \"%@\" to:", self.inputUrl.lastPathComponent];
+	[self observeTarget:self.treeController keyPath:@"selectedItem" options:0 block:^(MAKVONotification *note) {
+		[note.observer selectionChanged];
+	}];
 }
+
+#pragma mark - meat & potatos
+
+-(void)selectionChanged
+{
+	id selItem = self.treeController.selectedItem;
+	if ([selItem isKindOfClass:[RCWorkspace class]]) {
+		[self dismissSelf];
+		//we do the actual import
+		NSLog(@"imported %@ to %@", self.inputUrl.lastPathComponent, [selItem name]);
+	}
+}
+
+-(void)dismissSelf
+{
+	[self.presentingViewController dismissViewControllerAnimated:YES completion:^{
+		if (self.cleanupBlock)
+			self.cleanupBlock();
+	}];
+}
+
+#pragma mark - nav tree
 
 -(NSInteger)navTree:(AMNavigationTreeController*)navTree numberOfChildrenOfItem:(id)item
 {
 	if (nil == item)
 		return self.projects.count;
-	if ([item conformsToProtocol:@protocol(RCFileContainer)])
+	if ([item isKindOfClass:[RCProject class]])
+		return [[item workspaces] count];
+	if ([item isKindOfClass:[RCWorkspace class]])
 		return [[item files] count];
+	Rc2LogWarn(@"returning a default child count which should never happen");
 	return 0;
 }
 
 -(id)navTree:(AMNavigationTreeController*)navTree childOfItem:(id)item atIndex:(NSInteger)index
 {
+	id rs=nil;
 	if (nil == item)
-		return [self.projects objectAtIndex:index];
-	if ([item isKindOfClass:[RCProject class]])
-		return [[item workspaces] objectAtIndex:index];
-	return nil;
-}
-
--(void)navTree:(AMNavigationTreeController *)navTree willDisplayCell:(UITableViewCell *)cell forItem:(id)item
-{
-	NSLog(@"showing cell with check:%d", cell.accessoryType);
+		rs = [self.projects objectAtIndex:index];
+	else if ([item isKindOfClass:[RCProject class]])
+		rs = [[item workspaces] objectAtIndex:index];
+	return rs;
 }
 
 -(BOOL)navTree:(AMNavigationTreeController*)navTree isLeafItem:(id)item
@@ -77,20 +106,11 @@
 	return ![item isKindOfClass:[RCProject class]];
 }
 
--(IBAction)doCancel:(id)sender
-{
-	[self.presentingViewController dismissViewControllerAnimated:YES completion:^{
-		if (self.cleanupBlock)
-			self.cleanupBlock();
-	}];
-}
+#pragma mark - actions
 
--(IBAction)doImport:(id)sender
+-(IBAction)doDone:(id)sender
 {
-	[self.presentingViewController dismissViewControllerAnimated:YES completion:^{
-		if (self.cleanupBlock)
-			self.cleanupBlock();
-	}];
+	[self dismissSelf];
 }
 
 @end
