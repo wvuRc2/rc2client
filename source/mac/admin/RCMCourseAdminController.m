0c1e9652a0769e3e7857a1718175b66ddd4885d1
Mark Lilback <mark@lilback.com>
2013-05-14 14:56:15 -0400

    show basic list of courses in admin. fixed login bug after previous course changes

--- source/mac/admin/RCMCourseAdminController.m
+++ source/mac/admin/RCMCourseAdminController.m
@@ -13,6 +13,10 @@
 @property (nonatomic, copy) NSArray *semesters;
 @property (nonatomic, copy) NSArray *courses;
 @property (nonatomic, copy) NSArray *instances;
+@property (nonatomic, strong) IBOutlet NSArrayController *semesterController;
+@property (nonatomic, strong) IBOutlet NSArrayController *courseController;
+@property (nonatomic, strong) IBOutlet NSArrayController *instructorController;
+@property (nonatomic, strong) IBOutlet NSArrayController *instanceController;
 @end
 
 @implementation RCMCourseAdminController
@@ -28,9 +32,10 @@
 {
 	[[Rc2Server sharedInstance] fetchCourses:^(BOOL success, id results) {
 		if (success) {
-			self.semesters = [results objectForKey:@"semesters"];
-			self.courses = [results objectForKey:@"courses"];
-			self.instances = [results objectForKey:@"instances"];
+			self.semesterController.content = [results objectForKey:@"semesters"];
+			self.courseController.content = [results objectForKey:@"courses"];
+			self.instanceController.content = [results objectForKey:@"instances"];
+			self.instructorController.content = [results objectForKey:@"instructors"];
 		} else {
 			Rc2LogError(@"failed to fetch courses");
 		}
