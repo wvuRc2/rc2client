bdb000aecbf2cadad77cc5abcfad20fddb465823
Mark Lilback <mark@lilback.com>
2013-05-14 17:24:27 -0400

    fixed minor bugs with instructor names

--- source/mac/admin/RCMCourseAdminController.m
+++ source/mac/admin/RCMCourseAdminController.m
@@ -17,6 +17,7 @@
 @property (nonatomic, strong) IBOutlet NSArrayController *courseController;
 @property (nonatomic, strong) IBOutlet NSArrayController *instructorController;
 @property (nonatomic, strong) IBOutlet NSArrayController *instanceController;
+@property (nonatomic, strong) IBOutlet NSWindow *addDialog;
 @end
 
 @implementation RCMCourseAdminController
@@ -32,14 +33,34 @@
 {
 	[[Rc2Server sharedInstance] fetchCourses:^(BOOL success, id results) {
 		if (success) {
-			self.semesterController.content = [results objectForKey:@"semesters"];
-			self.courseController.content = [results objectForKey:@"courses"];
+			self.semesterController.content = [[results objectForKey:@"semesters"] sortedArrayUsingDescriptors:@[[NSSortDescriptor sortDescriptorWithKey:@"sortorder" ascending:NO]]];
+			self.courseController.content = [[results objectForKey:@"courses"] sortedArrayUsingDescriptors:@[[NSSortDescriptor sortDescriptorWithKey:@"shortname" ascending:YES]]];
 			self.instanceController.content = [results objectForKey:@"instances"];
 			self.instructorController.content = [results objectForKey:@"instructors"];
 		} else {
 			Rc2LogError(@"failed to fetch courses");
 		}
 	}];
+}
+
+- (void)didEndSheet:(NSWindow *)sheet returnCode:(NSInteger)returnCode contextInfo:(void *)contextInfo
+{
+	[sheet orderOut:self];
+}
+
+-(IBAction)showAddDialog:(id)sender
+{
+	[NSApp beginSheet:self.addDialog modalForWindow:self.view.window modalDelegate:self didEndSelector:@selector(didEndSheet:returnCode:contextInfo:) contextInfo:nil];
+}
+
+-(IBAction)addTheClzss:(id)sender
+{
+	[NSApp endSheet:self.addDialog returnCode:NSOKButton];
+}
+
+-(IBAction)cancelDialog:(id)sender
+{
+	[NSApp endSheet:self.addDialog returnCode:NSCancelButton];
 }
 
 @end
