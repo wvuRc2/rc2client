fbae54947478585dc3b4a8217540947ffa058399
Mark Lilback <mark@lilback.com>
2013-06-13 14:23:49 -0400

    mac session now shows project/wspace name. ios settings menu has more margin space

--- source/ios/AbstractTopViewController.m
+++ source/ios/AbstractTopViewController.m
@@ -64,7 +64,7 @@
 	}
 	if (nil == self.isettingsController) {
 		self.isettingsController = [[iSettingsController alloc] init];
-		self.isettingsController.contentSizeForViewInPopover = CGSizeMake(350, 440);
+		self.isettingsController.contentSizeForViewInPopover = CGSizeMake(350, 460);
 	}
 	UINavigationController *navController = [[UINavigationController alloc] initWithRootViewController:self.isettingsController];
 	self.isettingsPopover = [[UIPopoverController alloc] initWithContentViewController:navController];
