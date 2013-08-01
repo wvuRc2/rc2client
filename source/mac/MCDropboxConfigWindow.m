ffabe9f315a48d73c3ad1e5ef588ac918d2acb08
Mark Lilback <mark@lilback.com>
2013-08-01 17:00:59 -0400

    mac db config ui done. just need to implement saving changes.

--- source/mac/MCDropboxConfigWindow.m
+++ source/mac/MCDropboxConfigWindow.m
@@ -15,7 +15,7 @@
 @property (nonatomic, strong, readonly) DBMetadata *metadata;
 @property (nonatomic, copy, readonly) NSString *name;
 @property (copy) NSArray *children;
-@property (assign) BOOL loadedChildren;
+@property (readonly) BOOL childrenLoaded;
 @end
 
 @interface MCDropboxConfigWindow () <NSBrowserDelegate>
@@ -48,10 +48,14 @@
 		self.rootItems = [DBItem itemsforMetadataArray:metadata.contents];
 		[self.browser reloadColumn:0];
 	}];
+	[_browser setDoubleAction:@selector(saveChanges:)];
+	[self updateUI];
 }
 
 -(IBAction)saveChanges:(id)sender
 {
+	DBItem *item = [_browser itemAtIndexPath:_browser.selectionIndexPath];
+	NSLog(@"saving %@", item.metadata.path);
 	self.handler(1);
 }
 
@@ -65,10 +69,23 @@
 	self.handler(-1);
 }
 
+-(void)updateUI
+{
+	DBItem *item = [_browser itemAtIndexPath:_browser.selectionIndexPath];
+	[self.saveButton setEnabled:item != nil];
+}
+
 -(NSInteger)browser:(NSBrowser *)browser numberOfChildrenOfItem:(id)item
 {
 	if (nil == item)
 		return self.rootItems.count;
+	if (![item childrenLoaded]) {
+		NSInteger col = browser.lastColumn;
+		[DropBlocks loadMetadata:[[(DBItem*)item metadata] path] completionBlock:^(DBMetadata *metadata, NSError *error) {
+			[item setChildren:[DBItem itemsforMetadataArray:metadata.contents]];
+			[browser reloadColumn:col];
+		}];
+	}
 	return [[item children] count];
 }
 
@@ -89,6 +106,11 @@
 	return [item name];
 }
 
+-(void)browser:(NSBrowser *)browser didChangeLastColumn:(NSInteger)oldLastColumn toColumn:(NSInteger)column
+{
+	[self updateUI];
+}
+
 @end
 
 @implementation DBItem
@@ -96,8 +118,10 @@
 +(NSArray*)itemsforMetadataArray:(NSArray*)inArray
 {
 	NSMutableArray *out = [NSMutableArray arrayWithCapacity:inArray.count];
-	for (DBMetadata *md in inArray)
-		[out addObject:[[DBItem alloc] initWithMetadata:md]];
+	for (DBMetadata *md in inArray) {
+		if (md.isDirectory)
+			[out addObject:[[DBItem alloc] initWithMetadata:md]];
+	}
 	return out;
 }
 
@@ -117,6 +141,11 @@
 -(BOOL)isDirectory
 {
 	return _metadata.isDirectory;
+}
+
+-(BOOL)childrenLoaded
+{
+	return nil != _children;
 }
 
 @end
