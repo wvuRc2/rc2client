fae8ea56d69ea655c339434f327f898b4da10c47
Mark Lilback <mark@lilback.com>
2013-09-09 15:59:42 -0400

    image display looks much better

--- source/ios/ImageHolderView.m
+++ source/ios/ImageHolderView.m
@@ -22,8 +22,7 @@ static const CGFloat kKeyboardHeight = 354;
 @property (nonatomic, strong) IBOutlet UIButton *actionButton;
 @property (nonatomic, strong) IBOutlet UIButton *nameLabel;
 @property (nonatomic, strong) IBOutlet UILabel *dateLabel;
-@property (nonatomic, strong) NSDateFormatter *dateFormatter;
-@property (nonatomic, strong) UIToolbar *victimToolbar;
+@property (nonatomic, weak) IBOutlet NSLayoutConstraint *dateLabelWidthConstraint;
 @property (nonatomic, weak) CALayer *blayer;
 @end
 
@@ -32,7 +31,6 @@ static const CGFloat kKeyboardHeight = 354;
 {
 	[[NSBundle mainBundle] loadNibNamed:@"ImageHolderView" owner:self options:nil];
 	self.contentView.translatesAutoresizingMaskIntoConstraints = NO;
-//	self.translatesAutoresizingMaskIntoConstraints = NO;
 	[self addSubview:self.contentView];
 	[self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-[_contentView]-|" options:0 metrics:nil views:NSDictionaryOfVariableBindings(_contentView)]];
 	[self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-[_contentView]-|" options:0 metrics:nil views:NSDictionaryOfVariableBindings(_contentView)]];
@@ -41,104 +39,17 @@ static const CGFloat kKeyboardHeight = 354;
 	self.actionButton.tintColor = self.tintColor;
 	UIImage *actionImg = [[UIImage imageNamed:@"action"] imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate];
 	[self.actionButton setImage:actionImg forState:UIControlStateNormal];
-	self.dateLabel.backgroundColor = [UIColor orangeColor];
-	self.dateLabel.opaque = YES;
-/*	CGRect frame = self.frame;
-	frame.origin = CGPointZero;
-	frame.size.height -= 40;
-	frame.size.width -= 40;
-	frame.origin.y = 40;
-	frame.origin.x = 20;
-	UIScrollView *sv = [[UIScrollView alloc] initWithFrame:frame];
-	self.scrollView = sv;
-	[self addSubview:sv];
-	sv.delegate = self;
-	sv.minimumZoomScale = 0.25;
-	sv.maximumZoomScale = 4.0;
-	sv.showsHorizontalScrollIndicator=NO;
-	sv.showsVerticalScrollIndicator=NO;
-	sv.contentMode=UIViewContentModeCenter;
-	sv.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
-	UIImageView *iv = [[UIImageView alloc] initWithFrame:frame];
-	[sv addSubview:iv];
-	self.imageView = iv;
-	iv.frame = sv.bounds;
-//	[sv addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-[iv]-|" options:0 metrics:nil views:NSDictionaryOfVariableBindings(iv)]];
-//	[sv addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-[iv]-|" options:0 metrics:nil views:NSDictionaryOfVariableBindings(iv)]];
-	iv.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
-	iv.contentMode=UIViewContentModeCenter;
+	//make date label italic
+	UIFontDescriptor *fontD = [self.dateLabel.font.fontDescriptor fontDescriptorByAddingAttributes:@{UIFontDescriptorTraitsAttribute:@{UIFontSymbolicTrait:@(UIFontDescriptorTraitItalic)}}];
+	self.dateLabel.font = [UIFont fontWithDescriptor:fontD size:self.dateLabel.font.pointSize];
 
-
-	self.victimToolbar = [[UIToolbar alloc] initWithFrame:self.bounds];
-	CALayer *blayer =  self.victimToolbar.layer;
-	self.blayer = blayer;
-	CGRect barRect = self.bounds;
-	barRect.size.height = 38;
-	UIView *bv = [[UIView alloc] initWithFrame:barRect];
-//	[self insertSubview:bv belowSubview:self.imageView];
-	self.barView = bv;
-	bv.layer.borderWidth = 2;
-	bv.layer.borderColor = [UIColor yellowColor].CGColor;
-	bv.layer.masksToBounds = YES;
-	bv.backgroundColor = [UIColor blackColor];
-//	[self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|[bv]|" options:0 metrics:nil views:NSDictionaryOfVariableBindings(bv)]];
-//	bv.autoresizingMask = UIViewAutoresizingFlexibleWidth;
-	
-	UIButton *nf = [UIButton buttonWithType:UIButtonTypeCustom];
-	nf.frame = CGRectMake((barRect.size.width - 200)/2, 4, 200, 32);
-	self.nameLabel = nf;
-	nf.titleLabel.textAlignment = NSTextAlignmentCenter;
-//	nf.titleLabel.textColor = [UIColor whiteColor];
-	nf.tintColor = self.tintColor;
-	nf.translatesAutoresizingMaskIntoConstraints = NO;
-	nf.opaque = NO;
-	nf.backgroundColor = [UIColor clearColor];
-	[nf addTarget:self action:@selector(showImageSwitcher:) forControlEvents:UIControlEventTouchUpInside];
-	[bv addSubview:nf];
-
-	self.dateLabel = [[UILabel alloc] initWithFrame:CGRectMake(2, 16, 100, 20)];
-	[bv addSubview:self.dateLabel];
-	self.dateLabel.opaque=NO;
-	self.dateLabel.font = [UIFont preferredFontForTextStyle:UIFontTextStyleCaption2];
-	self.dateLabel.backgroundColor = [UIColor clearColor];
-	self.dateFormatter = [[NSDateFormatter alloc] init];
-	[self.dateFormatter setDateStyle:NSDateFormatterShortStyle];
-	[self.dateFormatter setTimeStyle:NSDateFormatterShortStyle];
-	self.dateLabel.layer.borderColor = [UIColor redColor].CGColor;
-	self.dateLabel.layer.borderWidth = 2;
-	self.dateLabel.translatesAutoresizingMaskIntoConstraints = NO;
-//	[bv addConstraint:[NSLayoutConstraint constraintWithItem:self.dateLabel attribute:NSLayoutAttributeBottom relatedBy:NSLayoutRelationEqual toItem:bv attribute:NSLayoutAttributeBottom multiplier:1 constant:-2]];
-	[bv addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:[_dateLabel]-2-|" options:0 metrics:nil views:NSDictionaryOfVariableBindings(_dateLabel)]];
-	CGRect btnFrame = CGRectMake(CGRectGetMaxX(bv.bounds)-31, 8, 23, 17);
-	UIButton *btn = [UIButton buttonWithType:UIButtonTypeCustom];
-	self.actionButton = btn;
-	[bv addSubview:btn];
-	btn.frame = btnFrame;
-//	btn.autoresizingMask = UIViewAutoresizingFlexibleLeftMargin;
-	btn.translatesAutoresizingMaskIntoConstraints = NO;
-	[btn setImage:[UIImage imageNamed:@"action"] forState:UIControlStateNormal];
-	[btn addTarget: self action:@selector(doActionMenu:) forControlEvents:UIControlEventTouchUpInside];
-	[bv addConstraint:[NSLayoutConstraint constraintWithItem:btn attribute:NSLayoutAttributeCenterY relatedBy:NSLayoutRelationEqual toItem:bv attribute:NSLayoutAttributeCenterY multiplier:1 constant:0]];
-	
-	[bv.layer insertSublayer:blayer atIndex:0];
-	self.layer.borderColor = [UIColor blackColor].CGColor;
-	self.layer.borderWidth = 1.0;
-
-	bv.translatesAutoresizingMaskIntoConstraints = NO;
-	[bv addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|[_dateLabel(100)]-2-[_nameLabel(>=40)]-2-[_actionButton]|" options:0 metrics:nil views:NSDictionaryOfVariableBindings(_dateLabel,_nameLabel,_actionButton)]];
-*/
-//	NSDictionary *vdict = NSDictionaryOfVariableBindings(_imageView);
-//	[self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|[_imageView(width)]|" options:0 metrics:@{@"width": @(_imageView.image.size.width)} views:vdict]];
-//	[self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|[_imageView(height)]|" options:0 metrics:@{@"height": @(_imageView.image.size.height)} views:vdict]];
 	self.imageView.translatesAutoresizingMaskIntoConstraints = NO;
-	self.layer.borderWidth = 2;
-	self.layer.borderColor = [UIColor blueColor].CGColor;
-	self.barView.layer.borderColor = [UIColor yellowColor].CGColor;
-	self.barView.layer.borderWidth = 2;
-	self.imageView.layer. borderWidth = 2;
-	self.imageView.layer.borderColor = [UIColor redColor].CGColor;
-	self.scrollView.layer.borderColor = [UIColor greenColor].CGColor;
-	self.scrollView.layer.borderWidth = 2;
+
+	self.barView.backgroundColor = [[UIColor lightGrayColor] colorWithAlphaComponent:0.2];
+	CIFilter *filter = [CIFilter filterWithName:@"CIGaussianBlur"];
+	self.barView.layer.filters = @[filter];
+	self.layer.backgroundColor = self.barView.backgroundColor.CGColor;
+	self.layer.filters = @[filter];
 }
 
 -(void)didMoveToWindow
@@ -233,20 +144,31 @@ static const CGFloat kKeyboardHeight = 354;
 
 -(void)setImage:(RCImage *)img
 {
-	static NSDateFormatter *dateFormatter;
+	static __strong NSDateFormatter *dateFormatter;
 	static dispatch_once_t onceToken;
 	dispatch_once(&onceToken, ^{
 		dateFormatter = [[NSDateFormatter alloc] init];
 		[dateFormatter setDateStyle:NSDateFormatterShortStyle];
 		[dateFormatter setTimeStyle:NSDateFormatterShortStyle];
 	});
-	if (img == _image)
+	self.actionButton.hidden = img == nil;
+	if (img && img == _image)
 		return;
 	_image = img;
 	[img image];
 	self.imageView.image = img.image;
-	[self.nameLabel setTitle:[img.name stringByDeletingPathExtension] forState:UIControlStateNormal];
-	self.dateLabel.text = [self.dateFormatter stringFromDate:[NSDate dateWithTimeIntervalSinceReferenceDate:img.timestamp]];
+	if (nil == img) {
+		UIFontDescriptor *fontD = [self.nameLabel.titleLabel.font.fontDescriptor fontDescriptorByAddingAttributes:@{UIFontDescriptorTraitsAttribute:@{UIFontSymbolicTrait:@(UIFontDescriptorTraitItalic)}}];
+		UIFont *fnt = [UIFont fontWithDescriptor:fontD size:12];
+		[self.nameLabel setAttributedTitle:[[NSAttributedString alloc] initWithString:@"tap to select image" attributes:@{NSFontAttributeName:fnt}] forState:UIControlStateNormal];
+		self.dateLabel.text = @"";
+		self.dateLabelWidthConstraint.constant = 40;
+	} else {
+		NSAttributedString *astr = [[NSAttributedString alloc] initWithString:[img.name stringByDeletingPathExtension]];
+		[self.nameLabel setAttributedTitle:astr forState:UIControlStateNormal];
+		self.dateLabelWidthConstraint.constant = 100;
+		self.dateLabel.text = [dateFormatter stringFromDate:[NSDate dateWithTimeIntervalSinceReferenceDate:img.timestamp]];
+	}
 	[self adjustImageDetails];
 	RunAfterDelay(0.3, ^{
 		_imageView.image = img.image;
