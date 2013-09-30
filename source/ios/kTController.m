72c74a55c93ae5e4b4007391822906a61ace8af7
Mark Lilback <mark@lilback.com>
2013-09-30 18:00:11 -0400

    half ass attempt at cycling through panels

--- source/ios/kTController.m
+++ source/ios/kTController.m
@@ -12,6 +12,7 @@
 @interface kTController ()
 @property (nonatomic, copy, readwrite) NSArray *panels;
 @property (nonatomic, strong, readwrite) UIView *view;
+@property (nonatomic, assign) NSUInteger currentPanelIndex;
 @end
 
 @implementation kTController
@@ -19,8 +20,13 @@
 - (id)init
 {
 	if ((self = [super init])) {
-		KTPanel *first = [[KTPanel alloc] initWithNibName:@"KTExecutePanel" controller:self];
-		self.panels = @[first];
+		NSMutableArray *panels = [NSMutableArray array];
+		for (NSString *aNib in @[@"KTExecutePanel", @"KTLatexPanel"]) {
+			KTPanel *panel = [[KTPanel alloc] initWithNibName:aNib controller:self];
+			[panels addObject:panel];
+		}
+		self.panels = panels;
+
 		self.view = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 1024, 53)];
 		[[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(orientationDidChange:) name:UIApplicationDidChangeStatusBarOrientationNotification object:nil];
 
@@ -28,7 +34,9 @@
 		[iview addSubview:self.view];
 		self.inputView = iview;
 
+		KTPanel *first = [panels objectAtIndex:0];
 		[self.view addSubview:first.view];
+		
 		[self orientationDidChange:nil]; //force initial sizing
 }
 	return self;
@@ -36,14 +44,56 @@
 
 -(void)nextPanel:(id)sender
 {
+	NSUInteger idx = self.currentPanelIndex + 1;
+	if (idx >= self.panels.count)
+		idx = 0;
+	KTPanel *oldPanel = self.panels[self.currentPanelIndex];
+	KTPanel *panel = self.panels[idx];
+	self.currentPanelIndex = idx;
+	CGRect frame = self.view.bounds;
+
+	//move newPanel offscreen w/o animation
+	CGRect r = frame;
+	r.origin.x += r.size.width;
+	panel.view.frame = r;
+	[self.view addSubview:panel.view];
+
+	[UIView animateWithDuration:0.5 animations:^{
+		CGRect oldRect = oldPanel.view.frame;
+		oldRect.origin.x -= oldRect.size.width + 100;
+		oldPanel.view.frame = oldRect;
+		panel.view.frame = frame;
+	} completion:^(BOOL finished) {
+		[oldPanel.view removeFromSuperview];
+	}];
 	
 }
 
 -(void)previousPanel:(id)sender
 {
-	
-}
+	NSInteger idx = self.currentPanelIndex - 1;
+	if (idx < 0)
+		idx = 0;
+	KTPanel *oldPanel = self.panels[self.currentPanelIndex];
+	KTPanel *panel = self.panels[idx];
+	self.currentPanelIndex = idx;
+	CGRect frame = self.view.bounds;
 
+	//move newPanel offscreen w/o animation
+	CGRect r = frame;
+	r.origin.x -= r.size.width;
+	panel.view.frame = r;
+	[self.view addSubview:panel.view];
+
+	[UIView animateWithDuration:0.5 animations:^{
+		CGRect oldRect = oldPanel.view.frame;
+		oldRect.origin.x += oldRect.size.width + 100;
+		oldPanel.view.frame = oldRect;
+		panel.view.frame = frame;
+	} completion:^(BOOL finished) {
+		[oldPanel.view removeFromSuperview];
+	}];
+}
 
 -(void)orientationDidChange:(NSNotification*)note
 {
