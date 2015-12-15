2cf940f8c6f49e81627011f5b7b865671d90323e
Mark Lilback <mark@lilback.com>
2015-12-15 14:53:06 -0500

    added test to make sure a duplicate workspace create fails

--- RestTests/Rc2RestServerTest.m
+++ RestTests/Rc2RestServerTest.m
@@ -21,7 +21,6 @@
 - (void)setUp
 {
 	[super setUp];
-	self.httpExpectation = [self expectationWithDescription:@"httpResponse"];
 	NSURLSessionConfiguration *config = [NSURLSessionConfiguration defaultSessionConfiguration];
 	self.server = [[Rc2RestServer alloc] initWithSessionConfiguration:config];
 	[self.server selectHost:@"localhost"];
@@ -35,11 +34,16 @@
 
 -(void)prepareURLSessionStub:(NSString*)jsonFileName testPath:(NSString*)path
 {
+	return [self prepareURLSessionStub:jsonFileName testPath:path status:200];
+}
+
+-(void)prepareURLSessionStub:(NSString*)jsonFileName testPath:(NSString*)path status:(int)statusCode
+{
 	[OHHTTPStubs stubRequestsPassingTest:^BOOL(NSURLRequest * _Nonnull request) {
 		return [request.URL.host isEqualToString:@"localhost" ] && [request.URL.path containsString:path];
 	} withStubResponse:^OHHTTPStubsResponse * _Nonnull(NSURLRequest * _Nonnull request) {
 		NSURL *url = [[NSBundle bundleForClass:[self class]] URLForResource:jsonFileName withExtension:@"json"];
-		return [OHHTTPStubsResponse responseWithFileURL:url statusCode:200 headers:@{@"Content-Type":@"application/json"}];
+		return [OHHTTPStubsResponse responseWithFileURL:url statusCode:statusCode headers:@{@"Content-Type":@"application/json"}];
 	}];
 }
 
@@ -52,6 +56,7 @@
 
 -(void)testLoginData
 {
+	self.httpExpectation = [self expectationWithDescription:@"httpResponse"];
 	[self prepareURLSessionStub:@"loginResults" testPath:@"login"];
 	[self.server login:@"test" password:@"beavis" handler:^(BOOL success, id results, NSError *error)
 	 {
@@ -67,6 +72,7 @@
 
 -(void)testCreateWorkspace
 {
+	self.httpExpectation = [self expectationWithDescription:@"httpResponse"];
 	__block id wspace;
 	[self prepareURLSessionStub:@"createWorkspace" testPath:@"workspaces"];
 	[self.server createWorkspace:@"foofy" completionBlock:^(BOOL success, id results, NSError *error)
@@ -77,6 +83,28 @@
 	 }];
 	[self waitForHttpExpectation];
 	XCTAssertEqualObjects([wspace valueForKey:@"name"], @"foofy", @"workspace names do not match");
+}
+
+-(void)testCreateDuplicateWorkspaceFails
+{
+	XCTestExpectation *loginEx = [self expectationWithDescription:@"login"];
+	[self prepareURLSessionStub:@"loginResults" testPath:@"login"];
+	[self.server login:@"test" password:@"beavis" handler:^(BOOL success, id results, NSError *error)
+	 {
+		 XCTAssertTrue(success, @"login failed: %@", error);
+		 [loginEx fulfill];
+	 }];
+	[self waitForExpectationsWithTimeout:2 handler:^(NSError * _Nullable error) {}];
+
+	XCTestExpectation *dupEx = [self expectationWithDescription:@"dup wspace"];
+	[self prepareURLSessionStub:nil testPath:@"workspaces" status:422];
+	[self.server createWorkspace:@"foofy" completionBlock:^(BOOL success, id results, NSError *error)
+	{
+		XCTAssertFalse(success, @"create workspace failed: %@", error);
+		XCTAssertEqual(error.code, 422, @"wrong error code for duplicate workspace");
+		[dupEx fulfill];
+	}];
+	[self waitForExpectationsWithTimeout:2 handler:^(NSError * _Nullable error) {}];
 }
 
 /*
