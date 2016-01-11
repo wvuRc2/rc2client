533f76812661b06d1930059e6c34d0621a6dcb5a
Mark Lilback <mark@lilback.com>
2016-01-11 10:18:11 -0500

    refactored test into multiple tests that use setUp to prep test objects

--- RestTests/Rc2SessionTests.swift
+++ RestTests/Rc2SessionTests.swift
@@ -10,41 +10,43 @@ import XCTest
 @testable import Rc2
 
 class Rc2SessionTests: XCTestCase {
+	static let wspaceJson = "[{\"id\":1, \"userId\":1, \"version\":1, \"name\":\"foofy\", \"files\":[]}]"
+	static let jsonData = wspaceJson.dataUsingEncoding(NSUTF8StringEncoding)
+	static let sjson = JSON(data: jsonData!)
+	var wspace:Rc2Workspace?
+	let delegate = SessionDelegate()
+	let wsSrc = MockWebSocket()
+	var session: Rc2Session?
 
 	override func setUp() {
 		super.setUp()
-		// Put setup code here. This method is called before the invocation of each test method in the class.
+		wspace = Rc2Workspace(json:Rc2SessionTests.sjson)
+		session = Rc2Session(wspace!, delegate:delegate, source:wsSrc)
 	}
 	
 	override func tearDown() {
-		// Put teardown code here. This method is called after the invocation of each test method in the class.
 		super.tearDown()
 	}
 
 	func testSessionCreation() {
-		let wspaceJson = "[{\"id\":1, \"userId\":1, \"version\":1, \"name\":\"foofy\", \"files\":[]}]"
-		let jsonData = wspaceJson.dataUsingEncoding(NSUTF8StringEncoding)
-		let sjson = JSON(data: jsonData!)
-		let wspace = Rc2Workspace(json:sjson)
-		let delegate = SessionDelegate()
-		let wsSrc = MockWebSocket()
-		let session = Rc2Session(wspace, delegate:delegate, source:wsSrc)
 		XCTAssertNotNil(session)
-		XCTAssertEqual(wspace, session.workspace)
-		XCTAssert(delegate === session.delegate)
-		
-		XCTAssertFalse(session.connectionOpen)
+		XCTAssertEqual(wspace, session!.workspace)
+		XCTAssert(delegate === session!.delegate)
+	}
+	
+	func testOpenCloseSession() {
+		XCTAssertFalse(session!.connectionOpen)
 		let openEx = expectationWithDescription("open session")
 		delegate.expectation = openEx
-		session.open()
+		session!.open()
 		waitForExpectationsWithTimeout(3) { (error) -> Void in
 		}
-		XCTAssertTrue(session.connectionOpen)
+		XCTAssertTrue(session!.connectionOpen)
 		delegate.expectation = expectationWithDescription("close session")
-		session.close()
+		session!.close()
 		waitForExpectationsWithTimeout(3) { (error) -> Void in
 		}
-		XCTAssertFalse(session.connectionOpen)
+		XCTAssertFalse(session!.connectionOpen)
 	}
 
 	@objc class SessionDelegate: NSObject, Rc2SessionDelegate {
